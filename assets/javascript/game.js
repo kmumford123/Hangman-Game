3dbd375383bdfd951b2772714c331eb45d5fe74a
Khamel Mumford <khamel.mumford@gmail.com>
2017-12-01 11:00:08 -0600

    TodaysUpdate

---
+++ assets/javascript/game.js
@@ -0,0 +1,30 @@
+// Set up a group of Answers
+	var correctAnswers = ["wolverine", "Beast", "Professor X", "Ice Man", "Storm", "Cyclops", "Jean Grey", "Colossus"];
+	var clue = ["Adamantium", "Smart Blue", "Mental Baldy", "Cool Breeze", "Ororo Munroe", "Too Hot Too Fly", "Man of Steel"];
+	var ChosenWord = Math.floor(Math.random()*correctAnswers.length);
+	for (var i = 0; i < correctAnswers.length; i++) {
+		console.log(correctAnswers[i]);
+	}
+	// var res = correctAnswers.split(correctAnswers[0]);
+	// console.out(res)
+	//Choose a word
+	document.onkeyup = function(event) {
+
+	}
+
+
+	//mozilla dev network - strings and arrays
+	//use .split and .join
+
+	//function -- sometimes has a name.
+	//layout function name (argument)
+	//place invocations in sequence
+	//functionName(arr)
+	//funcitonName(arr2)
+	//functionName(arr3)
+
+	//function has a list of arguments (could be empty) and has a return value
+
+// 	function fullName(firstName, middleName, lastName) {
+// 	... return firstName + " " + middleName " "+ " "+lastName;
+// ...}
