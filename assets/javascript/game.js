dd2c46decf5be76117ebc24d7281853bd49e5a28
Khamel Mumford <khamel.mumford@gmail.com>
2017-12-02 16:41:12 -0600

    Latest HW upload

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,9 +2,63 @@
 	var correctAnswers = ["wolverine", "Beast", "Professor X", "Ice Man", "Storm", "Cyclops", "Jean Grey", "Colossus"];
 	var clue = ["Adamantium", "Smart Blue", "Mental Baldy", "Cool Breeze", "Ororo Munroe", "Too Hot Too Fly", "Man of Steel"];
 	var ChosenWord = Math.floor(Math.random()*correctAnswers.length);
-	// for (var i = 0; i < correctAnswers.length; i++) {
+	var puzzle = [" "]
+	var guess = "c";
+	var NumberOfGuesses = 0;
+	var	GuessRemain = NumberOfGuesses - 1;
+
+		var correctAnswers = ["Wolverine", "Beast", "Professor X", "Ice Man", "Storm", "Cyclops", "Jean Grey", "Colossus"];
+	var clue = ["Adamantium", "Smart Blue", "Mental Baldy", "Cool Breeze", "Ororo Munroe", "Too Hot Too Fly", "Man of Steel"];
+// 	var index = Math.floor(Math.random()*correctAnswers.length);
+	var ChosenWord = correctAnswers[Math.floor(Math.random()*correctAnswers.length)];
+	var puzzle = " "
+for (var i = 0; i < ChosenWord.length; i++) {
+		puzzle += "_ ";
+		// console.log(puzzle);
+	}
+	console.log(puzzle);
+
+// Start the game
+	// Choose a Word
+		var clue = ["Adamantium", "Smart Blue", "Mental Baldy", "Cool Breeze", "Ororo Munroe", "Too Hot Too Fly", "Man of Steel"];
+		var ChosenWord = correctAnswers[Math.floor(Math.random()*correctAnswers.length)];
+
+	// Display a clue
+			//Use onclick function
+
+
+	// Display objective -- number of letters
+
+	//Issue number of Guesses -- set value of how many guesses the user has.
+
+
+
+	// allow User to make a guess
+		// When a user types a letter it issues a guess
+
+		//Track number of guesses -- when a user makes a guess, eliminate one available guess
+
+
+
+
+
+	// for (var i = 0; NumberOfGuesses < 10; i++) {
+	// 	if (NumberOfGuesses < 10); {
+	// 		alert(`You've used another guess, you have ${GuessRemain} remaining.`);
+	// 	}
+	// 		else {
+	// 			alert(`You have no guesses remaining.`);
+	// 		}
+	// 		}
+	// 	};
+	// }
+	for (var i = 0; i < ChosenWord.length; i++) {
+		puzzle += "_";
+		console.out(puzzle)
+
+	}
 	// 	console.log(correctAnswers[i]);
-	console.log(correctAnswers.split());
+	// console.log(wordstosplit.split(""));
 	// }
 	// var res = correctAnswers.split(correctAnswers[0]);
 	// console.out(res)
