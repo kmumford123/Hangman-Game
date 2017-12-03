774f14b4497fc22b915308a95a2b14118fac9181
Khamel Mumford <khamel.mumford@gmail.com>
2017-12-03 11:16:25 -0600

    Latest HW upload

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,59 +1,72 @@
 	// var correctAnswers = ["wolverine", "Beast", "Professor X", "Ice Man", "Storm", "Cyclops", "Jean Grey", "Colossus"];
-	var puzzle = [" "]
-	var guess = "c";
+	// var puzzle = [" "];
 
 // Start the game
 	// Start game on click
 	// Choose a Word
-		var puzzle = " "
-		var correctAnswers = ["wolverine", "Beast", "Professor X", "Ice Man", "Storm", "Cyclops", "Jean Grey", "Colossus"];
-		var chosenWord = correctAnswers[Math.floor(Math.random()*correctAnswers.length)];
+		// var puzzle = " "
+var correctAnswers = ["Wolverine", "Beast", "Professor X", "Ice Man", "Storm", "Cyclops", "Jean Grey", "Colossus"];
+var chosenWord = correctAnswers[Math.floor(Math.random()*correctAnswers.length)];
 
-	//  Display a clue -- Use onclick function
-		var clue = ["Adamantium", "Smart Blue", "Mental Baldy", "Cool Breeze", "Ororo Munroe", "Too Hot Too Fly", "Man of Steel"];
-
-	// Display objective -- show the current number of missing letters. (which should be all of them initially)
-		//Create a container using the 'empty-div id'
-		var puzzleDiv = document.getElementById("empty-div");
-		for (var i = 0; i < chosenWord.length; i++) {
-				  if (chosenWord[i] == " ") {
-				  	puzzle += "&nbsp;&nbsp;";
-				  }
-				  	else {
-				  		puzzle += "_ ";
-				  	}
+//  Display a clue -- Use onclick function
+var clue = ["Adamantium", "Smart Blue", "Mental Baldy", "Cool Breeze", "Ororo Munroe", "Too Hot Too Fly", "Man of Steel"];
+var clues = document.getElementById("clues"); 
+function clueCompare(){
+	for (var i = 0; i < correctAnswers.length; i++) {
+		 console.log(clue[i]);
 			}
-	// Create a element to display output in HTML
-	var newDiv = document.createElement("div");
-    newDiv.innerHTML = puzzle;
-     puzzleDiv.appendChild(newDiv);
-	console.log(puzzle);
-	console.log(chosenWord);
+		}
+	var newCluesDiv = document.createElement("div");
+    // newCluesDiv.innerHTML = clue;
+     clues.appendChild(newCluesDiv);
+	// console.log(clue);
 	console.out;
 
-	//Issue number of Guesses -- set value of how many guesses the user has.
-	var numberOfGuesses = 10;
-	var	guessRemain = numberOfGuesses - 1;
+// Display objective -- show the current number of missing letters. (which should be all of them initially)
+	//Create a container using the 'empty-div id'
+	var puzzleDiv = document.getElementById("empty-div");
+	var puzzle = [" "];
+	for (var j = 0; j < chosenWord.length; j++) {
+		if (chosenWord[j] == " ") {
+			  	puzzle += "&nbsp;&nbsp;";
+			  	}
+			  	else {
+			  		puzzle += "_ ";
+			  	}
+			  }	
+// Create a element to display output in HTML
+var newPuzzleDiv = document.createElement("div");
+newPuzzleDiv.innerHTML = puzzle;
+puzzleDiv.appendChild(newPuzzleDiv);
+console.log(puzzle);
+console.log(chosenWord);
+console.out;
+// newPuzzleDiv.attr("class", "fancy");
+
+//Issue number of Guesses -- set value of how many guesses the user has.
+var numberOfGuesses = 10;
+var	guessRemain = numberOfGuesses - 1;
 
 
-	// allow User to make a guess
-		// When a user types a letter it issues a guess
-		var win = 0;
-		var loss = 0;
-		
-		document.onkeyup = function (event) {
-			var guess = event.key;
-			var regexp = /[a-z]/gi;
+// allow User to make a guess
+	// When a user types a letter it issues a guess
+	var win = 0;
+	var loss = 0;
+	
+	document.onkeyup = function (event) {
+		var guess = event.key;
+		var regexp = /[a-z]/gi;
 			if (!regexp.test(guess)) {
 				// console.log("Success");
 				console.log("please enter a letter");
 			}
 			else {
+				loss = loss + 1;
 				win = win + 1;
-			document.getElementById("wincnt").innerHTML = win;
-			console.log(guess);
-			console.log(win)
+				document.getElementById("wincnt").innerHTML = win;
+				document.getElementById("lsscnt").innerHTML = loss;
 			}
+		}
 			// for (var i = 0; i < chosenWord.length; i++) {
 			// 	  if (chosenWord[i] == guess[j]) {
 			// 	  	puzzle += "&nbsp;&nbsp;";
@@ -63,13 +76,13 @@
 			// 	  	}
 			// if (guess[j] = chosenWord[j]) {
 			// 	puzzle += chosenWord[j]
-			}
+			// }
 		
 
 		//Track number of guesses -- when a user makes a guess, eliminate one available guess
 
 		// Track already used characters -- When the user issues a guess, 
-		}
+		// }
 
 
 
