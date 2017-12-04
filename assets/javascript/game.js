4de2efe61978f2304d4aa2f6492bf405710cacb7
Khamel Mumford <khamel.mumford@gmail.com>
2017-12-03 19:39:02 -0600

    Latest HW upload

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,49 +4,44 @@
 var correctAnswers = ["Wolverine", "Beast", "Professor X", "Ice Man", "Storm", "Cyclops", "Jean Grey", "Colossus"];
 var chosenWord = correctAnswers[Math.floor(Math.random()*correctAnswers.length)];
 
-//  Display a clue -- Use onclick function
-var clue = ["Adamantium", "Smart Blue", "Mental Baldy", "Cool Breeze", "Ororo Munroe", "4 Your Eyes Only", "Too Hot Too Fly", "Man of Steel"];
+//  Display a clue
+var possibleClue = ["Adamantium", "Smart Blue", "Mental Baldy", "Cool Breeze", "Ororo Munroe", "4 Your Eyes Only", "Too Hot Too Fly", "Man of Steel"];
+var hint = ""
 var clues = document.getElementById("clues"); 
 	for (var i = 0; i < correctAnswers.length; i++) {
 		 if (chosenWord === correctAnswers[i]) {
-		 	var clues = document.getElementById("clues");
-		 	var newCluesDiv = document.createElement("div");
-		    newCluesDiv.innerHTML = clue[i];
-		     clues.appendChild(newCluesDiv);
-		     console.log(clue[i]);
+		 	hint = possibleClue[i];
 			}
 		}
-	var newCluesDiv = document.createElement("div");
-    // newCluesDiv.innerHTML = showClue;
-     clues.appendChild(newCluesDiv);
-	// console.log(clue);
-	console.out;
+var clues = document.getElementById("clues");
+		 	var newCluesDiv = document.createElement("div");
+		    newCluesDiv.innerHTML = hint;
+		     clues.appendChild(newCluesDiv);
+		     console.log(hint);
 
 // Display objective -- show the current number of missing letters. (which should be all of them initially)
-	//Create a container using the 'empty-div id'
 	var puzzleDiv = document.getElementById("empty-div");
 	var puzzle = [" "];
 	for (var j = 0; j < chosenWord.length; j++) {
-		if (chosenWord[j] == " ") {
+		if (chosenWord[j] === " ") {
 			  	puzzle += "&nbsp;&nbsp;";
 			  	}
 			  	else {
 			  		puzzle += "_ ";
 			  	}
 			  }	
+
 // Create a element to display output in HTML
 var newPuzzleDiv = document.createElement("div");
 newPuzzleDiv.innerHTML = puzzle;
 puzzleDiv.appendChild(newPuzzleDiv);
 console.log(puzzle);
 console.log(chosenWord);
-console.out;
-// newPuzzleDiv.attr("class", "fancy");
+// console.out;
 
 //Issue number of Guesses -- set value of how many guesses the user has.
 var numberOfGuesses = 10;
-var	guessRemain = numberOfGuesses - 1;
-
+// var	guessRemain = numberOfGuesses - 1;
 
 // allow User to make a guess
 	// When a user types a letter it issues a guess
@@ -57,8 +52,20 @@ var	guessRemain = numberOfGuesses - 1;
 		var guess = event.key;
 		var regexp = /[a-z]/gi;
 			if (!regexp.test(guess)) {
-				// console.log("Success");
-				console.log("please enter a letter");
+				alert("please enter a letter");
+			}
+			else if {
+				var puzzle = [" "];
+				for (var k = 0; k < chosenWord.length; k++) {
+					if (chosenWord[k] === guess[k]) {
+						  	puzzle += guess[k];
+			  	}
+			  	else {
+			  		puzzle += "_ ";
+			  		numberOfGuesses = numberOfGuesses - 1;
+			  		}
+			  	}
+			  }	
 			}
 			else {
 				loss = loss + 1;
@@ -78,53 +85,8 @@ var	guessRemain = numberOfGuesses - 1;
 			// 	puzzle += chosenWord[j]
 			// }
 		
-
+// create a 
 		//Track number of guesses -- when a user makes a guess, eliminate one available guess
 
 		// Track already used characters -- When the user issues a guess, 
 		// }
-
-
-
-
-	// for (var i = 0; numberOfGuesses < 10; i++) {
-	// 	if (numberOfGuesses < 10); {
-	// 		alert(`You've used another guess, you have ${GuessRemain} remaining.`);
-	// 	}
-	// 		else {
-	// 			alert(`You have no guesses remaining.`);
-	// 		}
-	// 		}
-	// 	};
-	// }
-	// for (var i = 0; i < chosenWord.length; i++) {
-	// 	puzzle += "_";
-	// 	console.out(puzzle)
-
-	// }
-	// 	console.log(correctAnswers[i]);
-	// console.log(wordstosplit.split(""));
-	// }
-	// var res = correctAnswers.split(correctAnswers[0]);
-	// console.out(res)
-	//Choose a word
-	// document.onkeyup = function(event) {
-
-	// }
-
-
-	//mozilla dev network - strings and arrays
-	//use .split and .join
-
-	//function -- sometimes has a name.
-	//layout function name (argument)
-	//place invocations in sequence
-	//functionName(arr)
-	//funcitonName(arr2)
-	//functionName(arr3)
-
-	//function has a list of arguments (could be empty) and has a return value
-
-// 	function fullName(firstName, middleName, lastName) {
-// 	... return firstName + " " + middleName " "+ " "+lastName;
-// ...}
